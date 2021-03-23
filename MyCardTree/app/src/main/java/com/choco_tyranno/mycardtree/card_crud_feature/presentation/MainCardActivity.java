997726de6ab6c76cef775d288693b8cdd438ec63
choco-tyranno <choco.tyranno@gmail.com>
2021-03-23 21:51:33 +0900

    feat: add card/layer ui - #1

--- MyCardTree/app/src/main/java/com/choco_tyranno/mycardtree/card_crud_feature/presentation/MainCardActivity.java
+++ MyCardTree/app/src/main/java/com/choco_tyranno/mycardtree/card_crud_feature/presentation/MainCardActivity.java
@@ -1,16 +1,34 @@
 package com.choco_tyranno.mycardtree.card_crud_feature.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.choco_tyranno.mycardtree.R;
+import com.choco_tyranno.mycardtree.card_crud_feature.presentation.layer.LayerAdapter;
 
 public class MainCardActivity extends AppCompatActivity {
+    RecyclerView layerRV;
+    LayerAdapter layerAdapter;
+    LinearLayoutManager layerLM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_frame);
+        testInit();
+
+    }
+
+    void testInit(){
+        layerRV = findViewById(R.id.parent_recyclerview);
+        layerAdapter = new LayerAdapter();
+        layerAdapter.setHasStableIds(true);
+        layerLM = new LinearLayoutManager(MainCardActivity.this, LinearLayoutManager.VERTICAL, false);
+        layerRV.setLayoutManager(layerLM);
+        layerRV.setAdapter(layerAdapter);
+        layerAdapter.setTestData();
     }
 }
