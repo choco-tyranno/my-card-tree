d8f82dc9778092ab56237769fd130a30b0b32095
choco-tyranno <choco.tyranno@gmail.com>
2021-10-14 19:38:40 +0900

    feat: add SettingActivity.kt
    Add activity_setting.xml.

---
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.choco_tyranno.team_tree.presentation.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.choco_tyranno.team_tree.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
