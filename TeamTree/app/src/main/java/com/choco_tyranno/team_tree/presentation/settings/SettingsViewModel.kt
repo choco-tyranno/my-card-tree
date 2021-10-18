b45b29e3b9f3ea8776428ef766bc4112db1e7cd2
choco-tyranno <choco.tyranno@gmail.com>
2021-10-18 23:14:44 +0900

    chore: remove unnecessary dependencies in build.gradle
    -remove code <implementation "androidx.lifecycle:lifecycle-service:$lifecycle_version">.
    -remove code <implementation "androidx.lifecycle:lifecycle-process:$lifecycle_version">.
    -remove code <testImplementation "androidx.arch.core:core-testing:$arch_version">.
    -remove code <testImplementation "androidx.room:room-testing:$room_version">.

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
@@ -1,7 +1,6 @@
 package com.choco_tyranno.team_tree.presentation.settings
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 class SettingsViewModel : ViewModel() {
 
