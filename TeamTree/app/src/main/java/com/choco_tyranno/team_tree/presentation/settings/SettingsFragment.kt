3741a6a0a021ba5297f7f535a883971665fc7cb1
choco-tyranno <choco.tyranno@gmail.com>
2021-10-21 21:44:03 +0900

    feat: add property settingsViewModel.

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsFragment.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.choco_tyranno.team_tree.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    val settingsViewModel : SettingsViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
