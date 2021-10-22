9572ea2447cd0a120c1a6f2a9a3960a8db960293
choco-tyranno <choco.tyranno@gmail.com>
2021-10-22 16:16:59 +0900

    feat: add function openPageOssLicense()

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
@@ -1,7 +1,21 @@
 package com.choco_tyranno.team_tree.presentation.settings
 
+import android.content.Intent
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
+import com.choco_tyranno.team_tree.R
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class SettingsViewModel : ViewModel() {
+
+    fun openPageOssLicense(view : View){
+        view.context.startActivity(
+            Intent(view.context, OssLicensesMenuActivity::class.java)
+        )
+
+    }
 
 }
