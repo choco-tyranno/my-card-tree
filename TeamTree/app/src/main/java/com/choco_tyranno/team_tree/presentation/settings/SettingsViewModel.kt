3534ecf2709029b63c3b6127c39e057dd8425028
choco-tyranno <choco.tyranno@gmail.com>
2021-10-26 23:45:59 +0900

    feat: add code OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스")

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/settings/SettingsViewModel.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
 import com.choco_tyranno.team_tree.R
+import com.choco_tyranno.team_tree.presentation.MainCardActivity
+import com.google.android.gms.oss.licenses.OssLicensesActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 class SettingsViewModel : ViewModel() {
@@ -15,7 +17,7 @@ class SettingsViewModel : ViewModel() {
         view.context.startActivity(
             Intent(view.context, OssLicensesMenuActivity::class.java)
         )
-
+        OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스")
     }
 
 }
