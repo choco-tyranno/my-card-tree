d737ee2eb17f8b1b007c80d349e9ca63dba10ad9
choco-tyranno <choco.tyranno@gmail.com>
2021-11-02 21:29:21 +0900

    refactor: apply changed view structure
    -make data type clear by subdividing view hierarchy.

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/card_rv/OnClickListenerForMessageBtn.java
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/card_rv/OnClickListenerForMessageBtn.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class OnClickListenerForMessageBtn implements View.OnClickListener {
     @Override
-    public void onClick(View v) {
-        View targetViewItem = (View) v.getParent().getParent().getParent();
-        RecyclerView targetRecyclerView = (RecyclerView) targetViewItem.getParent();
-        String targetContactNumber = ((ContactCardViewHolder)targetRecyclerView.getChildViewHolder(targetViewItem)).getBinding().getCard().getContactNumber();
+    public void onClick(View messageBtn) {
+        MaterialCardView cardView  = (MaterialCardView) messageBtn.getParent();
+        ConstraintLayout cardFrame = (ConstraintLayout) cardView.getParent();
+        RecyclerView cardRecyclerView = (RecyclerView) cardFrame.getParent();
+        String targetContactNumber = ((ContactCardViewHolder)cardRecyclerView.getChildViewHolder(cardFrame)).getBinding().getCard().getContactNumber();
         Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
         smsIntent.setData(Uri.parse("smsto:"+targetContactNumber));
-        v.getContext().startActivity(smsIntent);
+        messageBtn.getContext().startActivity(smsIntent);
     }
 }
