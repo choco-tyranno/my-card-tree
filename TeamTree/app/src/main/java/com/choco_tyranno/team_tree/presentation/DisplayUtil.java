bb5b2b5a3a0296e4fa9c67e487f7890d16bb9cdd
choco-tyranno <choco.tyranno@gmail.com>
2021-11-05 16:49:13 +0900

    refactor: change code clear using specified dpi.

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/DisplayUtil.java
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/DisplayUtil.java
@@ -6,13 +6,49 @@ import android.content.res.Resources;
 
 public class DisplayUtil {
     public static int dpToPixel(Resources resources, int dp){
-        return dp*(int)resources.getDisplayMetrics().density;
+        return dp*(getDpi(resources)/160);
     }
+
     public static int pixelToDp(Resources resources, int pixel){
-        return pixel/(int)resources.getDisplayMetrics().density;
+        return pixel/(getDpi(resources)/160);
+    }
+
+    public static int getDpi(Resources resources){
+        float scale = resources.getDisplayMetrics().density;
+        int dpi = 0;
+        switch (String.valueOf(scale)){
+            case "1.0":
+                //mdpi
+                dpi = 160;
+                break;
+            case "1.5":
+                //hdpi
+                dpi = 240;
+                break;
+            case "2.0":
+                //xdpi
+                dpi = 320;
+                break;
+            case "3.0":
+                //xxdpi
+                dpi = 480;
+                break;
+            case "4.0":
+                //xxxdpi
+                dpi = 640;
+                break;
+            case "0.75":
+                //ldpi
+                dpi = 120;
+                break;
+            default:
+                throw new RuntimeException("DisplayUtil#dpToPixel : Proper scale not found");
+        }
+        return dpi;
     }
 
     public static int getScreenWidth(Context context){
+        //px
         return ((Activity) context).getWindowManager().getCurrentWindowMetrics().getBounds().right;
     }
 }
