84af441cc11e025c6e881188a4f88dd8d841e086
choco-tyranno <choco.tyranno@gmail.com>
2021-11-06 20:42:15 +0900

    feat: add method getScreenWidthAsDp()
    -refactor: change method getDpi() from using specified case with switch syntax to calculating proper dpi.

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/DisplayUtil.java
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/DisplayUtil.java
@@ -3,52 +3,28 @@ package com.choco_tyranno.team_tree.presentation;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
 
 public class DisplayUtil {
-    public static int dpToPixel(Resources resources, int dp){
-        return dp*(getDpi(resources)/160);
+    public static int dpToPixel(Resources resources, int dp) {
+        return dp * (getDpi(resources) / 160);
     }
 
-    public static int pixelToDp(Resources resources, int pixel){
-        return pixel/(getDpi(resources)/160);
+    public static int pixelToDp(Resources resources, int pixel) {
+        float divideResult = (float) getDpi(resources) / 160;
+        return (int) ((float) pixel / divideResult);
     }
 
-    public static int getDpi(Resources resources){
+    public static int getDpi(Resources resources) {
         float scale = resources.getDisplayMetrics().density;
-        int dpi = 0;
-        switch (String.valueOf(scale)){
-            case "1.0":
-                //mdpi
-                dpi = 160;
-                break;
-            case "1.5":
-                //hdpi
-                dpi = 240;
-                break;
-            case "2.0":
-                //xdpi
-                dpi = 320;
-                break;
-            case "3.0":
-                //xxdpi
-                dpi = 480;
-                break;
-            case "4.0":
-                //xxxdpi
-                dpi = 640;
-                break;
-            case "0.75":
-                //ldpi
-                dpi = 120;
-                break;
-            default:
-                throw new RuntimeException("DisplayUtil#dpToPixel : Proper scale not found");
-        }
-        return dpi;
+        return (int)(scale * 160);
     }
 
-    public static int getScreenWidth(Context context){
-        //px
+    public static int getScreenWidthAsPixel(Context context) {
         return ((Activity) context).getWindowManager().getCurrentWindowMetrics().getBounds().right;
+    }
+
+    public static int getScreenWidthAsDp(Context context) {
+        return pixelToDp(context.getResources(), getScreenWidthAsPixel(context));
     }
 }
