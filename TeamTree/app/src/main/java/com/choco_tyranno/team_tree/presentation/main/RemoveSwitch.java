f361ec8068056458124098f029924d44d84cf7d7
choco-tyranno <choco.tyranno@gmail.com>
2021-11-18 23:01:14 +0900

    feat: add RemoveSwitch.java

---
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/main/RemoveSwitch.java
@@ -0,0 +1,24 @@
+package com.choco_tyranno.team_tree.presentation.main;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+public class RemoveSwitch extends SwitchMaterial {
+    public RemoveSwitch(@NonNull Context context) {
+        super(context);
+    }
+
+    public RemoveSwitch(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RemoveSwitch(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
