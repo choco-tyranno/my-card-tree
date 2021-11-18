9b1588dbcfb579ead72dec4e385c4584b53167d8
choco-tyranno <choco.tyranno@gmail.com>
2021-11-18 23:45:31 +0900

    feat: add BottomBar.java

---
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/main/BottomBar.java
@@ -0,0 +1,29 @@
+package com.choco_tyranno.team_tree.presentation.main;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class BottomBar extends View {
+
+    public BottomBar(Context context) {
+        super(context);
+    }
+
+    public BottomBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BottomBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BottomBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+
+}
