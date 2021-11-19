8bc78f9a63eabec4f1bbaab3779e082a12c6f072
choco-tyranno <choco.tyranno@gmail.com>
2021-11-19 20:08:33 +0900

    feat: change DependentUIResolver field and add {static member class DependentUIResolverBuilder, DependentViewAction, DependentViewActionBuilder }

---
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/DependentUIResolver.java
@@ -0,0 +1,130 @@
+package com.choco_tyranno.team_tree.presentation;
+
+import android.view.View;
+
+import java.util.function.Consumer;
+
+public class DependentUIResolver<T extends View> {
+    private T baseView = null;
+    private DependentUIResolverBuilder.DependentViewAction<T> action = null;
+
+    private void setBaseView(T view) {
+        baseView = view;
+    }
+
+    public View getBaseView() {
+        return baseView;
+    }
+
+    private boolean isSetBaseView() {
+        return baseView != null;
+    }
+
+    private boolean isSetAction() {
+        return action != null;
+    }
+
+    private DependentUIResolver() {
+    }
+
+    private void setAction(DependentUIResolverBuilder.DependentViewAction<T> action) {
+        this.action = action;
+    }
+
+    public static class DependentUIResolverBuilder<T extends View> {
+        private DependentUIResolver<T> instance;
+
+        private void readyInstance() {
+            if (instance == null)
+                instance = new DependentUIResolver<T>();
+        }
+
+        public DependentUIResolverBuilder<T> baseView(T view) {
+            readyInstance();
+            instance.setBaseView(view);
+            return this;
+        }
+
+        @SafeVarargs
+        public final DependentUIResolverBuilder<T> with(int viewId, Consumer<T>... actions) {
+            readyInstance();
+            instance.setAction(
+                    new DependentViewAction.DependentViewActionBuilder<T>().
+                            viewId(viewId).actions(actions).build()
+            );
+            return this;
+        }
+
+        private boolean isBuildReady() {
+            return instance != null && instance.isSetBaseView() && instance.isSetAction();
+        }
+
+        public DependentUIResolver<T> build() {
+            if (isBuildReady())
+                return instance;
+            throw new RuntimeException("build fail -> trace : !isBuildReady");
+        }
+
+        private static class DependentViewAction<T extends View> {
+            private int viewId = -1;
+            private Consumer<T>[] actions = null;
+
+            private void setViewId(int viewId) {
+                this.viewId = viewId;
+            }
+
+            private void setActions(Consumer<T>[] actions) {
+                this.actions = actions;
+            }
+
+            private boolean isSetViewId() {
+                return viewId != -1;
+            }
+
+            private boolean isSetActions() {
+                return actions != null;
+            }
+
+            private static class DependentViewActionBuilder<T extends View> {
+                private DependentViewAction<T> instance;
+
+                private DependentViewActionBuilder() {
+                }
+
+                private DependentViewActionBuilder<T> viewId(int viewId) {
+                    readyInstance();
+                    instance.viewId = viewId;
+                    return this;
+                }
+
+                /*
+                 * Recommend :
+                 * Pass argument by method reference the view method.
+                 * */
+                private DependentViewActionBuilder<T> actions(Consumer<T>[] actions) {
+                    readyInstance();
+                    instance.actions = actions;
+                    return this;
+                }
+
+                private void readyInstance() {
+                    if (instance == null)
+                        instance = new DependentViewAction<T>();
+                }
+
+                private boolean isBuildReady() {
+                    return instance != null && instance.isSetActions() && instance.isSetViewId();
+                }
+
+                private DependentViewAction<T> build() {
+                    if (isBuildReady())
+                        return instance;
+                    throw new RuntimeException("DependentViewActionBuilder build fail : !isBuildReady");
+                }
+
+            }
+        }
+
+    }
+
+}
