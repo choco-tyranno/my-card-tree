95c032f541bcf2ac0655b6bd68c2c7c4a8cc0943
choco-tyranno <choco.tyranno@gmail.com>
2021-12-07 17:04:20 +0900

    feat: attach annotation AndroidEntryPoint to SplashActivity.kt

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/SplashActivity.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/SplashActivity.kt
@@ -3,7 +3,9 @@ package com.choco_tyranno.team_tree.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.choco_tyranno.team_tree.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
