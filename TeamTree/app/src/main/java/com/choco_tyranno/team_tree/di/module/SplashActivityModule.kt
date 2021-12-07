e2c6fe1b8362b0ccc2158c05207167a92219e069
choco-tyranno <choco.tyranno@gmail.com>
2021-12-07 17:29:32 +0900

    feat: add function 'provideAppUpdateManager()'

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/di/module/SplashActivityModule.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/di/module/SplashActivityModule.kt
@@ -1,11 +1,21 @@
 package com.choco_tyranno.team_tree.di.module
 
+import android.content.Context
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 @InstallIn(ActivityComponent::class)
 @Module
 object SplashActivityModule {
+
+    @Provides
+    fun provideAppUpdateManager(@ApplicationContext context : Context) : AppUpdateManager {
+        return AppUpdateManagerFactory.create(context);
+    }
 
 }
