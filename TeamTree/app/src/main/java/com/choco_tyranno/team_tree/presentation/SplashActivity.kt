cefc8775f36ba9c3ea3279144df056e07016cd58
choco-tyranno <choco.tyranno@gmail.com>
2021-12-07 17:31:15 +0900

    feat: add property appUpdateManager : AppUpdateManager

--- TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/SplashActivity.kt
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/presentation/SplashActivity.kt
@@ -2,13 +2,24 @@ package com.choco_tyranno.team_tree.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.choco_tyranno.team_tree.R
+import com.google.android.play.core.appupdate.AppUpdateManager
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    @Inject
+    lateinit var appUpdateManager: AppUpdateManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Log.d(TAG, "$appUpdateManager")
+    }
+
+    companion object{
+        val TAG = "@@Splash"
     }
 }
