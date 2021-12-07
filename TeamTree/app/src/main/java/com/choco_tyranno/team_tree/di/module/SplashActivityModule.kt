4812eb9e29d1eda588d20462f14bf113bf64ecdb
choco-tyranno <choco.tyranno@gmail.com>
2021-12-07 17:01:02 +0900

    feat: add SplashActivityModule.kt

---
+++ TeamTree/app/src/main/java/com/choco_tyranno/team_tree/di/module/SplashActivityModule.kt
@@ -0,0 +1,11 @@
+package com.choco_tyranno.team_tree.di.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@InstallIn(ActivityComponent::class)
+@Module
+object SplashActivityModule {
+
+}
